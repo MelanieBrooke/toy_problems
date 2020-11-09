c6a85a4c3a6769029ee7d51a5a9d39e65a80cfff
Melanie DeWitt <melaniebd1986@gmail.com>
2020-11-08 23:18:59 -0700

    Completed RPS toy problem for any number of rounds

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,41 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function (rounds) {
+  if (rounds > 10) {
+    return 'There are ' + (Math.pow(3, rounds)) + ' different possible combinations for this many rounds. Please enter a number 10 or lower to see all possible combinations.'
+  }
+  var result = [];
+  var total = Math.pow(3, rounds);
+  for (i = 0; i < total; i++) {
+    result.push('');
+  }
+
+  for (h = 0; h < rounds; h++) {
+    var incrementer = Math.pow(3, h)
+
+    var counter = 0;
+    for (j = 0; j < total; j++) {
+      if (counter %3 === 0 || counter === 0) {
+        result[j] += 'R';
+      } else if (counter %3 === 1 || counter === 1) {
+        result[j] += 'P';
+      } else if (counter %3 === 2 || counter === 2) {
+        result[j] += 'S';
+      }
+      if (j %incrementer === incrementer - 1 || j === incrementer - 1) {
+          counter += 1;
+      }
+    }
+  }
+
+  return result;
 };
 
+// var tester = rockPaperScissors(4);
+// console.log(tester);
+// var setTest = new Set();
+// for (j = 0; j < tester.length; j++) {
+//     setTest.add(tester[j]);
+//   }
+// console.log(setTest.size);
