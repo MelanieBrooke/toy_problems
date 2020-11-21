27e4213079087ffc64e9c0bef5f4ca89bc6c12b7
Melanie DeWitt <melaniebd1986@gmail.com>
2020-11-21 08:24:42 -0700

    Finished toy problem

--- nthFibonacci/nthFibonacci.js
+++ nthFibonacci/nthFibonacci.js
@@ -20,9 +20,43 @@
  *
  */
 
+// Recursive version
+
 var nthFibonacci = function (n) {
-  // TODO: implement me!
+  if (n === 0) {
+    return 0;
+  }
+  var addingMachine = function(x, y, counter) {
+    if (counter === 0) {
+      return y;
+    } else {
+      return addingMachine(y, (x + y), (counter - 1));
+    }
+  };
+  return addingMachine(0, 1, n-1);
 };
 
+// Iterative version
 
+var nthFibonacci = function (n) {
+  if (n === 0) {
+    return 0;
+  }
+  var x = 0;
+  var y = 1;
+  var z = 0;
+  for (i = 1; i < n; i++) {
+    z = x + y;
+    x = y;
+    y = z;
+  }
+  return y;
+};
+
+// console.log('should equal 1: ', nthFibonacci(1));
+// console.log('should equal 1: ', nthFibonacci(2));
+// console.log('should equal 2: ', nthFibonacci(3));
+// console.log('should equal 3: ', nthFibonacci(4));
+// console.log('should equal 5: ', nthFibonacci(5));
+// console.log('should equal 8: ', nthFibonacci(6));
 
