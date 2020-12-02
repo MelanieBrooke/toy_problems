612988362992582de195c7fad1e48a8217c2cdd3
Melanie DeWitt <melaniebd1986@gmail.com>
2020-12-02 15:54:44 -0700

    Completed commonCharacters BMR

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -14,4 +14,19 @@
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+  // Take care of edge cases
+  // Create result string
+  // Loop through string1
+  // if character is in string2 AND is not already in result string, add to result string
+  // return result string
+  if (arguments.length < 2) {
+    return undefined;
+  }
+  var commonChars = '';
+  for (i = 0; i < string1.length; i++) {
+    if (string2.includes(string1[i]) && !commonChars.includes(string1[i])) {
+      commonChars += string1[i];
+    }
+  }
+  return commonChars;
 };
