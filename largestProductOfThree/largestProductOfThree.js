747fc2c39fd220d031d52f8212e2e487f8d1ce3e
Melanie DeWitt <melaniebd1986@gmail.com>
2020-12-12 11:02:36 -0700

    Fixed largestOf3 to handle mixed positive and negative integers, complete with EC

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -19,50 +19,19 @@ var largestProductOfThree = function(array) {
     }
   }
 
-  var bubbleSort = function(array) {
-    var swaps = 0;
-    for (i = 0; i < array.length; i++) {
-      if (array[i + 1] !== undefined) {
-        var tempVal1 = array[i];
-        var tempVal2 = array[i + 1];
-        if (array[i] > array[i + 1]) {
-          array[i] = tempVal2;
-          array[i+1] = tempVal1;
-          swaps += 1;
+  var biggest = 0;
+  for (i = 0; i < array.length; i++) {
+    for (j =  i + 1; j < array.length; j++) {
+      for (k = j + 1; k < array.length; k++) {
+        var multiplied = (array[i] * array[j] * array[k]);
+        if (multiplied > biggest) {
+          biggest = multiplied;
         }
       }
     }
-    if (swaps === 0) {
-      return array;
-    } else {
-      return bubbleSort(array);
-    }
-  };
-
-  var sortedArray = bubbleSort(array);
-  var largest = sortedArray[sortedArray.length - 1];
-  var secondLargest = sortedArray[sortedArray.length - 2];
-  var thirdLardest = sortedArray[sortedArray.length - 3];
-  return largest * secondLargest * thirdLardest;
+  }
+  return biggest;
 };
-
-// var largestProductOfThree = function(array) {
-//   if (array.length < 3) {
-//     if (array.length === 2) {
-//       return array[0] * array[1];
-//     } else if (array.length === 1) {
-//       return (array[0]);
-//     } else {
-//       return 0;
-//     }
-//   }
-//   var sortedArray = array.sort();
-//   var largest = sortedArray[sortedArray.length - 1];
-//   var secondLargest = sortedArray[sortedArray.length - 2];
-//   var thirdLardest = sortedArray[sortedArray.length - 3];
-//   return largest * secondLargest * thirdLardest;
-// };
-
 
 // input: array of numbers
 // output: number that is the product of the largest three numbers in the array
@@ -72,8 +41,6 @@ var largestProductOfThree = function(array) {
 // handle edge cases:
 // for array of 2, multiply them. Array of 1, return that number. Empty array, return 0
 
-// sort the numbers, then take the biggest three
-// Use your bubble sort if regular sort can't handle negatives
-// create variables for largest, second largest, and third largest
-// multiply those three numbers together
-// return answer
+// create result variable max
+// iterate through the numbers, multiplying them by combos of the other numbers
+// if the answer is bigger than max, change max
