519a5ef8c1573eff6533c1663ea6e98c250abf63
Melanie DeWitt <melaniebd1986@gmail.com>
2020-12-19 10:40:45 -0700

    Completed toy problem reverseInteger

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,34 @@
-/* 
-* 
+/*
+*
 * Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
+*
+* Given a positive integer, return its digits reversed.
+*
+* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
 * - Only use integers and math in your solution.
 *
 */
 
 function reverseInteger(number){
-  // TODO: Implement this function!
+  if (typeof number !== 'number' || number < 1) {
+    return undefined;
+  }
+  TBRnumber = Math.floor(number);
+  var reverseNum = 0;
+  while (TBRnumber > 0) {
+    var lastDigit = TBRnumber % 10;
+    reverseNum = reverseNum * 10 + lastDigit;
+    TBRnumber = Math.floor(TBRnumber / 10);
+  }
+  return reverseNum;
 }
+
+
+
+
+
+// Input: positive integer (number)
+// Output: positive integer (number), reversed from input
+// Constraints: Must only use math/integers, no converting input into strings or arrays
+// Maybe don't mutate original input number
+// Edge cases: Do I need to worry about negative numbers, non integers, etc? They should all return null or undefined? Am I even tested on those?
