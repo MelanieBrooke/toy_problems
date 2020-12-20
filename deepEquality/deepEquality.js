b8d41e55babd2fe99b438bc4f3cfa102fd6d3533
Melanie DeWitt <melaniebd1986@gmail.com>
2020-12-19 20:53:16 -0700

    Toy problem deepEquality almost working, return statement not always exiting function

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,49 @@
   * don't worry about handling cyclical object structures.
   *
   */
-var deepEquals = function(apple, orange) {
+
+ var deepEquals = function(apple, orange) {
+  if (Object.keys(apple).length !== Object.keys(orange).length) {
+    return false;
+  }
+  for (var key in apple) {
+    if (orange[key] === undefined) {
+      return false;
+    }
+    if (typeof apple[key] !== typeof orange[key]) {
+      return false;
+    }
+    if (typeof apple[key] === 'number' || typeof apple[key] === 'string') {
+      console.log('apple',apple[key], 'is a',typeof apple[key]);
+      console.log('orange',orange[key],'is a', typeof orange[key]);
+      if (apple[key] !== orange[key]) {
+        console.log('they do not match');
+        return false;
+      }
+    }
+    if (Array.isArray(apple[key]) && Array.isArray(orange[key])) {
+      console.log('add handle arrays functionality later');
+    }
+    if (typeof apple[key] === 'object' && typeof orange[key] === 'object') {
+      console.log('objects!')
+      deepEquals(apple[key], orange[key]);
+    }
+  }
+  return true;
 };
+
+
+
+// console.log(deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}})); // true
+console.log(deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}})); // false
+// console.log(deepEquals({a:1}, {b:1})) // false
+// console.log(deepEquals({a:1}, {a:2})) // false
+
+///////////////////////////////////////////////////////////////////////
+// recursive function
+// first check length
+// then for each property key, see if matching value exists
+// if value is an object, repeat
+// Add functionality for array?
+
+// code still in snippets
