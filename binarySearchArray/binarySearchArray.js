0389de7fe8f1d6fe0e85c1c3a6ba5af951b614d2
Melanie DeWitt <melaniebd1986@gmail.com>
2021-01-09 15:57:39 -0700

    Completed toy problem binarySearchArray

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,40 @@
  */
 
 var binarySearch = function (array, target) {
-};
+  var minIndex = 0;
+  var maxIndex = array.length - 1;
+  var midIndex = Math.floor((maxIndex + minIndex) / 2);
+  var targetIndex = null;
 
+  var checkMidpoint = function(min, max, mid) {
+    if (array[mid] === target) {
+      targetIndex = midIndex;
+      return;
+    } else if (array[min] === target) {
+      targetIndex = minIndex;
+      return;
+    } else if (array[max] === target) {
+      targetIndex = maxIndex;
+      return;
+    } else if (max - min === 1) {
+      return;
+    } else {
+      if (array[mid] > target) {
+        maxIndex = mid;
+        midIndex =Math.floor((maxIndex + minIndex) / 2);
+        if (maxIndex !== minIndex) {
+          checkMidpoint(minIndex, maxIndex, midIndex);
+        }
+      } else if (array[mid] < target) {
+        minIndex = mid;
+        midIndex = Math.floor((maxIndex + minIndex) / 2);
+        if (minIndex !== maxIndex) {
+          checkMidpoint(minIndex, maxIndex, midIndex);
+        }
+      }
+    }
+  };
+
+  checkMidpoint(minIndex, maxIndex, midIndex);
+  return targetIndex;
+};
