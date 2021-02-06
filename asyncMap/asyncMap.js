ba599641ffa507307aca1dc3ce44bf999e7cdeda
Melanie DeWitt <melaniebd1986@gmail.com>
2021-02-06 11:15:57 -0700

    complete async map

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -37,6 +37,54 @@
  *
  */
 
+// input: array of functions with their own callbacks, then a final callback
+// each function runs and the results must display in the order the functions were listed in
+// then the final callback is called on the results
+
+// ATTEMPT TO USE PROMISES
+
+// var asyncMap = function(tasks, callback) {
+//   var taskResults = [];
+
+//   Promise.all(tasks)
+//   .then((values) => {taskResults.push(values)});
+//   // .then((values) => {console.log(values)})
+
+//   var results = callback(taskResults);
+//   return results;
+// };
 
 var asyncMap = function(tasks, callback) {
+  var taskResults = [];
+  var taskNum = tasks.length;
+
+  tasks.map((task, idNum) => {
+    task((result) => {
+      taskResults[idNum] = result;
+      taskNum -= 1;
+      if (taskNum === 0) {
+        var finalResult = callback(taskResults);
+        return finalResult;
+      }
+    })
+  })
 };
+
+
+// asyncMap([
+//     function(cb){
+//     setTimeout(function(){
+//       cb('one');
+//     }, 200);
+//   },
+//   function(cb){
+//     setTimeout(function(){
+//       cb('two');
+//     }, 100);
+//   }
+//  ],
+//   function(results){
+//     // the results array will equal ['one','two'] even though
+//     // the second function had a shorter timeout.
+//     console.log(results); // ['one', 'two']
+//  });
