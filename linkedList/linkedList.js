d3ed0a14206f8be8fbb48f9612fdffe8c6eac96d
Melanie DeWitt <melaniebd1986@gmail.com>
2021-03-13 16:00:01 -0700

    linkedlist functions madenode, addtotail, and contains complete

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -20,22 +20,62 @@
 
 
 var LinkedList = function() {
-  //fill me in!
+  this.head = null;
+  this.tail = null;
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-) {
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = this.makeNode(value);
+  if (this.head === null) {
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+  this.tail.next = newNode;
+  this.tail = newNode;
+  }
 };
 
 LinkedList.prototype.removeHead = function() {
 };
 
-LinkedList.prototype.contains = function(
-) {
+LinkedList.prototype.contains = function(value) {
+  var contains = false;
+  var check = function(node) {
+    if (node.value === value) {
+      contains = true;
+    } else if (node.next !== null) {
+      check(node.next);
+    }
+  }
+  if (this.head !== null) {
+    check(this.head);
+  }
+  return contains;
 };
 
-LinkedList.prototype.makeNode = function(
-) {
+LinkedList.prototype.makeNode = function(value) {
+  var node = {
+    value: value,
+    next: null
+  }
+  return node;
 };
+
+var list = new LinkedList();
+list.tail;         //yields 'null'
+list.addToTail(4);
+list.addToTail(5);
+console.log(list.head.value);   //yields '4';
+console.log(list.contains(5));  //yields 'true';
+console.log(list.contains(6));  //yields 'false';
+// list.removeHead(); //yields '4';
+console.log(list.tail.value);   //yields '5';
+// list.removeHead(); //yields '5';
+// list.removeHead(); //yields 'null';
+
+// inputs: addToTail: a user-inputted value (type not specified); removeHead (none); contains (value to search for, type not specified)
+// outputs: addToTail: none; removeHead: the value that was at the head and is being removed (type not specified); contains: boolean declaring whether the supplied value is in the linked list or not
+// constraints: pseudoclassical structure
+// edge cases: calling removeHead when there is nothing in the list should return 'null,' asking for the tail or head when the list is empty should return 'null'
