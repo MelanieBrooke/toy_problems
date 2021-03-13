4bf8a4191ae4050b9a73dc7d57d4abc07352ad33
Melanie DeWitt <melaniebd1986@gmail.com>
2021-03-13 16:28:40 -0700

    complete linkedList

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -20,15 +20,15 @@
 
 
 var LinkedList = function() {
-  this.head = null;
-  this.tail = null;
+  this.head = {value:null};
+  this.tail = {value:null};
 };
 
 //write methods here!
 
 LinkedList.prototype.addToTail = function(value) {
   var newNode = this.makeNode(value);
-  if (this.head === null) {
+  if (this.head.value === null) {
     this.head = newNode;
     this.tail = newNode;
   } else {
@@ -38,6 +38,15 @@ LinkedList.prototype.addToTail = function(value) {
 };
 
 LinkedList.prototype.removeHead = function() {
+  if (this.head === null || this.head.value === null) {
+    return null;
+  }
+  var oldHead = this.head.value;
+  this.head = this.head.next;
+  if (this.head === null) {
+    this.tail.value = null;
+  }
+  return oldHead;
 };
 
 LinkedList.prototype.contains = function(value) {
@@ -49,7 +58,7 @@ LinkedList.prototype.contains = function(value) {
       check(node.next);
     }
   }
-  if (this.head !== null) {
+  if (this.head.value !== null) {
     check(this.head);
   }
   return contains;
@@ -63,17 +72,18 @@ LinkedList.prototype.makeNode = function(value) {
   return node;
 };
 
-var list = new LinkedList();
-list.tail;         //yields 'null'
-list.addToTail(4);
-list.addToTail(5);
-console.log(list.head.value);   //yields '4';
-console.log(list.contains(5));  //yields 'true';
-console.log(list.contains(6));  //yields 'false';
-// list.removeHead(); //yields '4';
-console.log(list.tail.value);   //yields '5';
-// list.removeHead(); //yields '5';
-// list.removeHead(); //yields 'null';
+// var list = new LinkedList();
+// list.tail;         //yields 'null'
+// list.addToTail(4);
+// list.addToTail(5);
+// console.log(list.head.value);   //yields '4';
+// console.log(list.contains(5));  //yields 'true';
+// console.log(list.contains(6));  //yields 'false';
+// console.log(list.removeHead()); //yields '4';
+// console.log('tail is', list.tail.value);   //yields '5';
+// console.log(list.removeHead()); //yields '5';
+// console.log(list.removeHead()); //yields 'null';
+// console.log('tail now', list.tail.value)
 
 // inputs: addToTail: a user-inputted value (type not specified); removeHead (none); contains (value to search for, type not specified)
 // outputs: addToTail: none; removeHead: the value that was at the head and is being removed (type not specified); contains: boolean declaring whether the supplied value is in the linked list or not
