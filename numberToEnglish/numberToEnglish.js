82087ed61c6a4d5a58312bf4bc122c2e068c8081
Melanie DeWitt <melaniebd1986@gmail.com>
2021-05-09 16:54:40 -0600

    complete numberToEnglish (no decimals)

--- numberToEnglish/numberToEnglish.js
+++ numberToEnglish/numberToEnglish.js
@@ -54,5 +54,106 @@ var numbersToPlace = {
 };
 
 Number.prototype.toEnglish = function () {
-  // return my value as english words
+  var integer = Number(this.toString());
+  if (numbersToWords[integer]) {
+    return numbersToWords[integer];
+  } else if (numbersToPlace[integer]) {
+      return numbersToPlace[integer];
+  }
+  var words = '';
+  var string = this.toString();
+  var strings = [];
+
+  if (string.length %3 === 1) {
+    string = '00' + string;
+  } else if (string.length %3 === 2)  {
+      string = '0' + string;
+  }
+
+  for (i = 0; i < string.length; i+=3) {
+    place = string[i] + string[i+1] + string[i+2];
+    strings.push(place);
+  }
+
+  var places = strings.length - 1;
+
+  var wordIt = (num) => {
+    englishNum = '';
+    var digitHun = Number(num[0]);
+    var digitTen = Number(num[1]);
+    var digitOne = Number(num[2]);
+    if (digitHun > 0) {
+      englishNum += numbersToWords[digitHun] + ' hundred';
+    }
+    if (digitHun > 0 && digitTen > 0 || digitHun > 0 && digitTen === 0 && digitOne > 0) {
+      englishNum += ' ';
+    }
+    if (digitTen === 1) {
+      var digitTeen = digitTen * 10 + digitOne;
+      englishNum += numbersToWords[digitTeen];
+      return englishNum;
+    }
+    if (digitTen > 0) {
+      englishNum += numbersToWords[digitTen * 10];
+    }
+    if (digitTen > 0 && digitOne > 0) {
+      englishNum += '-'
+    }
+    if (digitOne > 0) {
+      englishNum += numbersToWords[digitOne];
+    }
+    englishNum += ' ';
+    return englishNum;
+  }
+
+  var placeIt = (index) => {
+    var toWord = places - index;
+    if (toWord === 0) {
+      return '';
+    } else if (toWord === 1) {
+      return 'thousand ';
+    } else if (toWord === 2) {
+      return 'million ';
+    } else if (toWord === 3) {
+      return 'billion ';
+    } else if (toWord === 4) {
+      return 'trillion ';
+    } else if (toWord === 5) {
+      return 'quadrillion ';
+    } else if (toWord === 6) {
+      return 'quintillion ';
+    }
+  }
+
+  for (var j = 0; j < strings.length; j++) {
+    if (strings[j] !== '000') {
+      words += wordIt(strings[j]);
+      words += placeIt(j);
+    }
+  }
+  if (words[words.length-1] === ' ') {
+    words = words.slice(0, words.length-1);
+  };
+  return words;
 };
+
+
+// input: integer (if doing extra credit, the input could also be a float)
+// output: a string with the English written-out version of the number
+// constraints: none
+// edge cases: non-number input
+// notes: need to remember to add dashes
+
+// maybe break it up into sets of three numbers
+// write a function that just handles three numbers at a time
+// then add the correct 'million/quintillion/etc' based on how many sets
+// before any calculating, see if the number is in the sets given
+//
+
+// console.log((7).toEnglish()); // > "seven"
+// console.log((575).toEnglish()); // > "five hundred seventy-five"
+// console.log((78193512).toEnglish()); // > "seventy-eight million one hundred ninety-three thousand five hundred twelve"
+// console.log((3052750903).toEnglish());
+// console.log((284842750392057375073).toEnglish())
+// console.log((0).toEnglish());
+// console.log((38420000235).toEnglish());
