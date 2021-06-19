baa84dc9584212acdacf30eced275725ed2d41a4
Melanie DeWitt <melaniebd1986@gmail.com>
2021-06-19 11:05:11 -0600

    complete sortableTable if using sortable is allowed

--- sortableTable/sortableTable.js
+++ sortableTable/sortableTable.js
@@ -8,8 +8,27 @@
  * Use the table provided in index.html.
  **/
 
+// const $ = require('jquery');
+
+// var $itemName = $('th').contains('Item Name');
+// console.log($itemName);
+
 $(function () {
-  // TODO: your code here!
+  $('#myTable').addClass('sortable');
+  // placeholder click in case I have to refactor to not use sortable
+  // $('th').click(function() {
+  //   console.log('clicked');
+  // })
+ // easily call individual headers
+  // var $item = $("th:contains('Item Name')");
+  // var $pounds = $("th:contains('Number of Pounds')");
+  // var $price = $("th:contains('Price Per Pound')");
+  // var $date = $("th:contains('Expiration Date')");
 
 });
 
+
+
+// Cannot edit HTML table directly
+// Jquery can add classes - perhaps add classes to the things in the columns?
+// Am I supposed to add package.json or no??
